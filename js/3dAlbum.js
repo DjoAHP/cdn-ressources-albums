396c1e4f450fab1abd7ab5cf6c9d20fbe85cfe83
DjoAHP <djodjo.vely@gmail.com>
2026-02-08 20:19:29 +0100

    📝 MODIF JS

--- js/3dAlbum.js
+++ js/3dAlbum.js
@@ -1,4 +1,4 @@
-export const render3DAlbum = (projectName) => {
+const render3DAlbum = (projectName) => {
   const BASE_URL =
     "https://cdn.jsdelivr.net/gh/DjoAHP/cdn-ressources-albums@main/images/";
 
@@ -74,3 +74,5 @@ export const render3DAlbum = (projectName) => {
     document.body.classList.toggle("disabled"),
   );
 };
+
+render3DAlbum("are-you-experienced");
